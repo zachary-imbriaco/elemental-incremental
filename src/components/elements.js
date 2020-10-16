e9f41eabd112a61cbc47f33a0746a6c37ec696fd
Vaeliwen <zaman@imbriaco.com>
2020-10-16 13:41:27 -0400

    More work trying to set up multiple automatic incrementations.

--- src/components/elements.js
+++ src/components/elements.js
@@ -4,103 +4,93 @@ import '../styles.css'
 
 import { incrementElement, increaseSpeed } from '../actions/actions';
 
-class ElementsBar extends React.Component {
+class ElementBar extends React.Component {
 
-    fireTick = () => {
-        this.props.incrementElement('fire')
+    
+
+    tick = () => {
+        this.props.incrementElement(this.props.element)
     }
-    waterTick = () => {
-        this.props.incrementElement('water')
-    }
-    earthTick = () => {
-        this.props.incrementElement('earth')
-    }
-    airTick = () => {
-        this.props.incrementElement('air')
+    
+    increment = (e) => {
+        e.preventDefault()
+        this.props.incrementElement(this.props.element)
     }
 
-    incrementFire = (e) => {
+    speedUp = (e) => {
         e.preventDefault()
-        this.props.incrementElement('fire')
-    }
-    incrementWater = (e) => {
-        e.preventDefault()
-        this.props.incrementElement('water')
-    }
-    incrementEarth = (e) => {
-        e.preventDefault()
-        this.props.incrementElement('earth')
-    }
-    incrementAir = (e) => {
-        e.preventDefault()
-        this.props.incrementElement('air')
+        this.props.increaseSpeed(this.props.element)
     }
 
-    speedUpFire = (e) => {
-        e.preventDefault()
-        this.props.increaseSpeed('fire')
-    }
-    speedUpWater = (e) => {
-        e.preventDefault()
-        this.props.increaseSpeed('water')
-    }
-    speedUpEarth = (e) => {
-        e.preventDefault()
-        this.props.increaseSpeed('earth')
-    }
-    speedUpAir = (e) => {
-        e.preventDefault()
-        this.props.increaseSpeed('air')
-    }
+
 
     render() {
-        return (
-            <div className='elements-component'>
-                <div className='elements-list'>
-                    <p>Current fire: {this.props.fire}</p>
-                    <p>Current water: {this.props.water}</p>
-                    <p>Current earth: {this.props.earth}</p>
-                    <p>Current air: {this.props.air}</p>
+        if (this.props.element == 'fire') {
+            return (
+                <div className='elements-component'>
+                    <div className='elements-list'>
+                        <p>Current {this.props.element}: {this.props.fire} </p>
+                    </div>
+                    <div className='elements-buttons'>
+                        <button onClick={this.increment}>Add {this.props.element}</button>
+                    </div>
+                    <div className='elements-list'>
+                        <p>{this.props.element}/second: {this.props.fireSpeed}</p>
+                    </div>
+                    <div className='elements-buttons'>
+                        <button onClick={this.speedUp}>Increase {this.props.element} Speed: {this.props.fSpeedCost} {this.props.element}</button>
+                    </div>
                 </div>
-                <div className='elements-buttons'>
-                    <button onClick={this.incrementFire}>Add Fire</button>
-                    <button onClick={this.incrementWater}>Add Water</button>
-                    <button onClick={this.incrementEarth}>Add Earth</button>
-                    <button onClick={this.incrementAir}>Add Air</button>
+            )
+        }
+        if (this.props.element == 'water') {
+            return (
+                <div className='elements-component'>
+                    <div className='elements-list'>
+                        <p>Current {this.props.element}: {this.props.water} </p>
+                    </div>
+                    <div className='elements-buttons'>
+                        <button onClick={this.increment}>Add {this.props.element}</button>
+                    </div>
+                    <div className='elements-list'>
+                        <p>{this.props.element}/second: {this.props.waterSpeed}</p>
+                    </div>
+                    <div className='elements-buttons'>
+                        <button onClick={this.speedUp}>Increase {this.props.element} Speed: {this.props.wSpeedCost} {this.props.element}</button>
+                    </div>
                 </div>
-                <div className='elements-list'>
-                <p>Fire Speed: {this.props.fireSpeed}</p>
-                <p>Water Speed: {this.props.waterSpeed}</p>
-                <p>Earth Speed: {this.props.earthSpeed}</p>
-                <p>Air Speed: {this.props.airSpeed}</p>
-                </div>
-                <div className='elements-buttons'>
-                    <button onClick={this.speedUpFire}>Increase Fire Speed: {this.props.fSpeedCost} Fire</button>
-                    <button onClick={this.speedUpWater}>Increase Water Speed: {this.props.wSpeedCost} Water</button>
-                    <button onClick={this.speedUpEarth}>Increase Earth Speed: {this.props.eSpeedCost} Earth</button>
-                    <button onClick={this.speedUpAir}>Increase Air Speed: {this.props.aSpeedCost} Air</button>
-                </div>
-            </div>
-        )
+            )
+        }
+
     }
 
     // Need to figure out how to tick multiple things at once + how to prevent tick from resetting on update while still preventing recursion.
     componentDidUpdate() {
-        if (this.props.fireSpeed > 0) {
-            clearInterval(this.tickFire)
-            this.tickFire = setInterval(this.fireTick, 1000 / this.props.fireSpeed)
+        if (this.props.element === 'fire') {
+            clearInterval(this.fireTock)
+            if (this.props.fireSpeed >= 1) {
+            this.fireTock = setInterval(this.tick, 1000 / this.props.fireSpeed)
+            }
+            else {
+                console.log(this.props.fireSpeed)
+            }
         }
-        if (this.props.waterSpeed > 0) {
-            clearInterval(this.tickWater)
-            this.tickWater = setInterval(this.waterTick, 1000 / this.props.waterSpeed)
+        if (this.props.element === 'water') {
+            clearInterval(this.waterTock)
+            if (this.props.waterSpeed >= 1) {
+            this.waterTock = setInterval(this.tick, 1000 / this.props.waterSpeed)
+            }
+            else {
+                console.log(this.props.waterSpeed)
+            }
         }
-        if (this.props.earthSpeed > 0) {
-            clearInterval(this.tickEarth)
-            this.tickEarth = setInterval(this.earthTick, 1000 / this.props.earthSpeed)
+        if (this.props.element === 'earth') {
+            clearInterval(this.earthTock)
+            this.earthTock = setInterval(this.tick, 1000 / this.props.earthSpeed)
         }
-        if (this.props.airSpeed > 0) {
-            clearInterval(this.tickAir)
-            this.tickAir = setInterval(this.airTick, 1000 / this.props.airSpeed)
+        if (this.props.element === 'air') {
+            clearInterval(this.airTock)
+            this.airTock = setInterval(this.tick, 1000 / this.props.airSpeed)
         }
     }
 
@@ -120,12 +110,10 @@ const mapStateToProps = state => {
         eSpeedCost: state.eSpeedCost,
         air: state.air,
         airSpeed: state.airSpeed,
-        aSpeedCost: state.aSpeedCost,
-
-    }
+        aSpeedCost: state.aSpeedCost
+        }
 }
-
 export default connect(
     mapStateToProps, 
     { incrementElement, increaseSpeed }
-)(ElementsBar)
+)(ElementBar)
